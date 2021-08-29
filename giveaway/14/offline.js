f66a9d58350f666631ab37a9cf34cad8972b9762
danaAlcala <bigdalcala@gmail.com>
2021-08-29 11:01:01 -0700

    yet another giveaway

---
+++ giveaway/14/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1630260025,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/btnenter-sheet0.png",
+		"images/solidplatformandwall-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/lava-sheet0.png",
+		"images/btntryagain-sheet0.png",
+		"images/movingplatform-sheet0.png",
+		"images/btnleft-sheet0.png",
+		"images/btnright-sheet0.png",
+		"images/btnjump-sheet0.png",
+		"images/bluevoid-sheet0.png",
+		"media/explode2.m4a",
+		"media/explode2.ogg",
+		"media/gamemusic1.m4a",
+		"media/gamemusic1.ogg",
+		"media/gamemusic2.m4a",
+		"media/gamemusic2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
